c2e607235cea242d4d111e1580914f1b82c8d566
Jun Kwon <102341633+kj2063@users.noreply.github.com>
2026-02-27 15:04:35 +0900

    아이디찾기, 비밀번호 찾기 팝업 수정

--- src/main/java/com/miso/lxnn/dao/FindUserDao.java
+++ src/main/java/com/miso/lxnn/dao/FindUserDao.java
@@ -1,15 +1,11 @@
 package com.miso.lxnn.dao;
 
 import com.miso.lxnn.domain.User;
-import com.miso.lxnn.dto.master.UserChangePasswordDto;
 import org.apache.ibatis.annotations.Mapper;
-
-import java.util.List;
 
 @Mapper
 public interface FindUserDao {
 
     User findUserId(User user);
     User findUserPassword(User user);
-    void updatePassword(User user);
 }
