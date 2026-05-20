c1c8f1d20ce4fac9d62d676fe7f218702a3d6b79
jjhjgh <jjhjgh123@gmail.com>
2026-05-20 15:47:25 +0900

    수정

---
+++ src/main/java/com/kcabEvent/dto/participant/ParticipantEventTypeSaveDto.java
@@ -0,0 +1,11 @@
+package com.kcabEvent.dto.participant;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ParticipantEventTypeSaveDto {
+    private Long eventParticipantSeq;
+    private String participantTypeCd;
+}
---
+++ src/main/java/com/kcabEvent/dto/participant/ParticipantTypeOptionDto.java
@@ -0,0 +1,12 @@
+package com.kcabEvent.dto.participant;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ParticipantTypeOptionDto {
+    private String value;
+    private String label;
+    private Integer sortSeq;
+}
